c0253c60f63ce87ba29e164a5ceb6b1c481d7566
Jason Jiannuo Pei <peijiannuo@gmail.com>
2023-02-01 13:40:43 -0500

    Added stub software library, see #4

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bzLock',
+    version='1.0',
+    description='bzLock Library',
+    author='Jason Pei, Thomas Zheng',
+    author_email='peijiannuo@gmail.com, tz1372@nyu.edu',
+    url='https://github.com/tzheng1372/bzLock',
+    py_modules=['bzLock'],
+    )
