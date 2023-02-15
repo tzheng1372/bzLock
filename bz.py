9758d0e99b05788f01da0da2aae0834a8f1ce2a8
tzheng1372 <tz1372@nyu.edu>
2023-02-15 13:22:28 -0500

    made changes to fsr functions and added code to load cell functions

--- bz.py
+++ bz.py
@@ -8,32 +8,44 @@ from hx711 import HX711
 
 ADC_Start = 0b00000001
 ADC_CH0 = 0b10000000
-
+FSR_pin = 23
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.mode = 0b00
+spi.max_speed_hz = 1200000
 
 def callback_fn(FSR_pin):
-    print("phone detected")
+    return True
 
-def detect_phone():
+def fsr_detect_phone():
     GPIO.setmode(GPIO.BCM)
-    FSR_pin = 23
-
     GPIO.setup(FSR_pin, GPIO.IN)
     GPIO.add_event_detect(FSR_pin, GPIO.RISING, callback = callback_fn)
 
-def get_weight():
-    spi = spidev.SpiDev()
-    spi.open(0,0)
-    spi.mode = 0b00
-    spi.max_speed_hz = 1200000
+def fsr_adc_detect_phone():
     readBytes = spi.xfer2([ADC_Start, ADC_CH0, 0x00])
     digitalValue = (((readBytes[1] & 0b11) << 8) | readBytes[2])
     print(format(readBytes[2], '#010b'), format(readBytes[1], '#010b'), format(readBytes[0], '#010b'))
     print(digitalValue)
+    if digitalValue > 0:
+        return True
+    else:
+        return False
 
 hx = HX711(dout_pin = 5, pd_sck_pin = 6)
 
-def get_weight():
-    pass
+def load_cell_get_weight():
+    hx.power_up()
+    hx.zero()
+    if hx._ready():
+        weight = hx.get_weight_mean()
+    return weight
 
-def detect_phone():
-    pass
+
+def load_cell_detect_phone():
+    if hx._ready():
+        weight = hx.get_weight_mean()
+    if weight > 0:
+        return True
+    else:
+        return False
