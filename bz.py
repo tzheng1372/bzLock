9d5de12dba2fea31d5237d91bbb340be41965555
Jason Jiannuo Pei <peijiannuo@gmail.com>
2023-02-21 23:03:14 -0500

    finish bz.py and add functions to demo.py

--- bz.py
+++ bz.py
@@ -1,6 +1,8 @@
 import board
 import busio
 import digitalio
+import pigpio
+import RPi.GPIO as GPIO
 import spidev
 import smbus
 import time
@@ -8,22 +10,35 @@ import time
 from hx711 import HX711
 from PIL import Image, ImageDraw, ImageFont
 
-import RPi.GPIO as GPIO
 import adafruit_matrixkeypad
 import adafruit_ssd1306
 
 # OLED specs
 OLED_WIDTH = 128
 OLED_HEIGHT = 64
-OLED_ADDRESS = 0x3c
+OLED_ADDRESS = 0x3C
 OLED_REGADDR = 0x00
 OLED_DISPOFF = 0xAE
 OLED_DISPON  = 0xAF
 
-# Initialize I2C library busio
+# Initialize smbus and I2C library busio
 bus = smbus.SMBus(1)
 i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c, addr=OLED_ADDRESS)
+
+# Initialize servo motor
+pwm_pin = 12
+pi = pigpio.pi()
+
+# Initialize 
+ADC_Start = 0b00000001
+ADC_CH0 = 0b10000000
+FSR_pin = 23
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.mode = 0b00
+spi.max_speed_hz = 1200000
+hx = HX711(dout_pin = 5, pd_sck_pin = 6)
 
 def callback_fn(FSR_pin):
     return True
@@ -55,13 +70,12 @@ def load_cell_detect_phone():
     weight = hx.get_weight_mean()
     if weight > 0:
         return True
-    else:
-        return False
+    return False
 
 def load_cell_shut_down():
     hx.power_down()
 
-def get_numpad_input():
+def numpad_get_input():
     cols = [digitalio.DigitalInOut(x) for x in (board.D26, board.D20, board.D21)]
     rows = [digitalio.DigitalInOut(x) for x in (board.D5, board.D6, board.D13, board.D19)]
     keys = ((1, 2, 3), (4, 5, 6), (7, 8, 9), ("*", 0, "#"))
@@ -70,10 +84,10 @@ def get_numpad_input():
     while True:
         keys = keypad.pressed_keys
         if keys:
-            print("Pressed: ", keys)
-            return keys
+            print("Pressed:", keys[0])
+            return keys[0]
 
-def oled_text(text):
+def oled_update(text):
     # Graphics stuff - create a canvas to draw/write on
     image = Image.new("1", (oled.width, oled.height))
     draw = ImageDraw.Draw(image)
@@ -101,7 +115,6 @@ def oled_off():
 def oled_on():
     bus.write_byte_data(OLED_ADDRESS, OLED_REGADDR, OLED_DISPON)
 
-def position_servo(angle):
-    #on = (angle/180) + 1
-    #duty = on/20
-    pi.hardware_PWM(12, 59, (angle/180)*1000000)
+def servo_position(angle):
+    inc = int(angle * 100000 / 180)
+    pi.hardware_PWM(pwm_pin, 50, 50000 + inc)
