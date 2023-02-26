ac8f03be1cfe3a4b85ffb73a1026c42bcd74cf54
Jason Jiannuo Pei <peijiannuo@gmail.com>
2023-02-26 17:50:13 -0500

    small update for display functions

--- bz.py
+++ bz.py
@@ -10,63 +10,72 @@ from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 
-# Initialize OLED
-serial = i2c(port=1, address=0x3C)
-device = sh1106(serial)
+display = None
 
 # Initialize FSR
 ADC_Start = 0b00000001
 ADC_CH0 = 0b10000000
 FSR_pin = 23
 spi = spidev.SpiDev()
-spi.open(0,0)
+spi.open(0, 0)
 spi.mode = 0b00
 spi.max_speed_hz = 1200000
 
 # Initialize LC
-hx = HX711(dout_pin = 17, pd_sck_pin = 27)
+hx = HX711(dout_pin=17, pd_sck_pin=27)
 
 # Initialize servo motor
 pwm_pin = 12
 pi = pigpio.pi()
 
+
 def callback_fn(FSR_pin):
     return True
+
 
 def fsr_detect_phone():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(FSR_pin, GPIO.IN)
-    GPIO.add_event_detect(FSR_pin, GPIO.RISING, callback = callback_fn)
+    GPIO.add_event_detect(FSR_pin, GPIO.RISING, callback=callback_fn)
+
 
 def fsr_adc_detect_phone():
     readBytes = spi.xfer2([ADC_Start, ADC_CH0, 0x00])
     digitalValue = (((readBytes[1] & 0b11) << 8) | readBytes[2])
-    print(format(readBytes[2], '#010b'), format(readBytes[1], '#010b'), format(readBytes[0], '#010b'))
+    print(format(readBytes[2], '#010b'), format(
+        readBytes[1], '#010b'), format(readBytes[0], '#010b'))
     print(digitalValue)
     if digitalValue > 0:
         return True
     return False
 
+
 def load_cell_setup():
     hx.power_up()
     hx.zero()
 
+
 def load_cell_get_weight():
     weight = hx.get_weight_mean()
     return weight
-    
+
+
 def load_cell_detect_phone():
     weight = hx.get_weight_mean()
     if weight > 0:
         return True
     return False
 
+
 def load_cell_shut_down():
     hx.power_down()
 
+
 def numpad_get_input():
-    cols = [digitalio.DigitalInOut(x) for x in (board.D26, board.D20, board.D21)]
-    rows = [digitalio.DigitalInOut(x) for x in (board.D5, board.D6, board.D13, board.D19)]
+    cols = [digitalio.DigitalInOut(x)
+            for x in (board.D26, board.D20, board.D21)]
+    rows = [digitalio.DigitalInOut(x) for x in (
+        board.D5, board.D6, board.D13, board.D19)]
     keys = ((1, 2, 3), (4, 5, 6), (7, 8, 9), ("*", 0, "#"))
     keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
@@ -76,10 +85,21 @@ def numpad_get_input():
             print("Pressed:", keys[0])
             return keys[0]
 
-def oled_update(text):
-    with canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline="white", fill="black")
+
+def setup_display():
+    display = sh1106(i2c(port=1, address=0x3C))
+
+
+def clear_display():
+    with canvas(display) as draw:
+        draw.rectangle(display.bounding_box, outline="white", fill="black")
+
+
+def text_to_display(text):
+    with canvas(display) as draw:
+        draw.rectangle(display.bounding_box, outline="white", fill="black")
         draw.text((30, 40), text, fill="white")
+
 
 def servo_position(angle):
     inc = int(angle * 100000 / 180)
--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ import time
 # load_cell_get_weight()
 # load_cell_shut_down()
 
-while(1):
+while (1):
     print("enter 4 digits:")
     a = bz.numpad_get_input()
     time.sleep(0.3)
@@ -19,12 +19,8 @@ while(1):
     time.sleep(0.3)
     print(a, b, c, d)
 
-    print("screen is off")
-    bz.oled_off()
-    time.sleep(1)
-    print("screen is on")
-    bz.oled_on()
-    time.sleep(1)
-    print("type something to show on display:")
-    bz.oled_update(input())
-    time.sleep(0.3)
+    bz.setup_display()
+    bz.text_to_display("text")
+    time.sleep(2)
+    bz.clear_display()
+    time.sleep(2)
