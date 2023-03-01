d26d874b0e78c0d4fe70ed04f335c38f89763d06
tzheng1372 <tz1372@nyu.edu>
2023-03-01 12:07:02 -0500

    added two more methods to the class and commented out the old functions

--- bz.py
+++ bz.py
@@ -15,63 +15,66 @@ from luma.oled.device import sh1106
 # Initialize FSR
 ADC_Start = 0b00000001
 ADC_CH0 = 0b10000000
-FSR_pin = 23
-spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.mode = 0b00
-spi.max_speed_hz = 1200000
+# FSR_pin = 23
+# spi = spidev.SpiDev()
+# spi.open(0, 0)
+# spi.mode = 0b01
+# spi.max_speed_hz = 1200000
 
 # Initialize LC
-hx = HX711(dout_pin=17, pd_sck_pin=27)
+#hx = HX711(dout_pin=17, pd_sck_pin=27)
 
 
-def callback_fn(FSR_pin):
-    return True
+# def callback_fn(FSR_pin):
+#     return True
 
 
-def fsr_detect_phone():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(FSR_pin, GPIO.IN)
-    GPIO.add_event_detect(FSR_pin, GPIO.RISING, callback=callback_fn)
+# def fsr_detect_phone():
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setup(FSR_pin, GPIO.IN)
+#     GPIO.add_event_detect(FSR_pin, GPIO.RISING, callback=callback_fn)
 
 
-def fsr_adc_detect_phone():
-    readBytes = spi.xfer2([ADC_Start, ADC_CH0, 0x00])
-    digitalValue = (((readBytes[1] & 0b11) << 8) | readBytes[2])
-    print(format(readBytes[2], '#010b'), format(
-        readBytes[1], '#010b'), format(readBytes[0], '#010b'))
-    print(digitalValue)
-    if digitalValue > 0:
-        return True
-    return False
+# def fsr_adc_detect_phone():
+#     readBytes = spi.xfer2([ADC_Start, ADC_CH0, 0x00])
+#     digitalValue = (((readBytes[1] & 0b11) << 8) | readBytes[2])
+#     print(format(readBytes[2], '#010b'), format(
+#         readBytes[1], '#010b'), format(readBytes[0], '#010b'))
+#     print(digitalValue)
+#     if digitalValue > 2:
+#         return True
+#     return False
+
+# hx = HX711(dout_pin=5, pd_sck_pin=6)
+
+# def load_cell_setup():
+#     hx.power_up()
+#     hx.reset()
+#     hx.zero()
 
 
-def load_cell_setup():
-    hx.power_up()
-    hx.reset()
-    hx.zero()
+# def load_cell_get_weight():
+#     weight = hx.get_weight_mean(10)
+#     print(weight)
+#     return weight
 
 
-def load_cell_get_weight():
-    weight = hx.get_weight_mean()
-    return weight
+# def load_cell_detect_phone():
+#     weight = hx.get_weight_mean(10)
+#     if weight > -10000:
+#         return True
+#     return False
 
 
-def load_cell_detect_phone():
-    weight = hx.get_weight_mean()
-    if weight > 0:
-        return True
-    return False
-
-
-def load_cell_shut_down():
-    hx.power_down()
+# def load_cell_shut_down():
+#     hx.power_down()
 
 
 class bzLock:
     def __init__(self):
         self.display = None
         self.numpad = None
+        self.spi = None
 
     def setup_display(self):
         try:
@@ -127,3 +130,19 @@ class bzLock:
         pwm_pin = 12
         inc = int(angle * 100000 / 180)
         pigpio.pi().hardware_PWM(pwm_pin, 50, 50000 + inc)
+
+    def setup_spi(self):
+        self.spi = spidev.SpiDev()
+        self.spi.open(0,0)
+        self.spi.mode = 0b01
+        self.spi.max_speed_hz = 1200000
+
+    def detect_phone(self):
+        readBytes = self.spi.xfer2([ADC_Start, ADC_CH0, 0x00])
+        digitalValue = (((readBytes[1] & 0b11) << 8) | readBytes[2])
+        print(format(readBytes[2], '#010b'), format(
+            readBytes[1], '#010b'), format(readBytes[0], '#010b'))
+        print(digitalValue)
+        if digitalValue > 2:
+            return True
+        return False
