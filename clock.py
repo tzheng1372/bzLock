b39f386b4889b66cea79b6a84df921a48f40558d
Jason Jiannuo Pei <peijiannuo@gmail.com>
2023-04-05 13:23:07 -0400

    added clock.py

---
+++ clock.py
@@ -0,0 +1,66 @@
+import math
+import time
+import datetime
+from luma.core.render import canvas
+
+import bz
+
+bz = bz.bzLock()
+bz.setup_display()
+device = bz.display
+
+
+def posn(angle, arm_length):
+    dx = int(math.cos(math.radians(angle)) * arm_length)
+    dy = int(math.sin(math.radians(angle)) * arm_length)
+    return (dx, dy)
+
+
+def main():
+    today_last_time = "Unknown"
+    while True:
+        now = datetime.datetime.now()
+        today_date = now.strftime("%d %b %y")
+        today_time = now.strftime("%H:%M:%S")
+        if today_time != today_last_time:
+            today_last_time = today_time
+            with canvas(device) as draw:
+                now = datetime.datetime.now()
+                today_date = now.strftime("%d %b %y")
+
+                margin = 4
+
+                cx = 30
+                cy = min(64, 64) / 2  # device.height
+
+                left = cx - cy
+                right = cx + cy
+
+                hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
+                hrs = posn(hrs_angle, cy - margin - 7)
+
+                min_angle = 270 + (6 * now.minute)
+                mins = posn(min_angle, cy - margin - 2)
+
+                sec_angle = 270 + (6 * now.second)
+                secs = posn(sec_angle, cy - margin - 2)
+
+                draw.ellipse((left + margin, margin, right - margin,
+                             min(128, 64) - margin), outline="white")  # device.height
+                draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
+                draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
+                draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
+                draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2),
+                             fill="white", outline="white")
+                draw.text((2 * (cx + margin), cy - 8),
+                          today_date, fill="yellow")
+                draw.text((2 * (cx + margin), cy), today_time, fill="yellow")
+
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
