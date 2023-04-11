28f8708ce3fbd75274ed86116ea00214e43b2bb1
Jason Jiannuo Pei <peijiannuo@gmail.com>
2023-04-11 00:56:36 -0400

    Add main.py with clock

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import datetime
 import time
 import math
 
+sleeping = True
+
 display_lock = Lock()
 
 bz = bzLock()
@@ -19,43 +21,47 @@ def posn(angle, arm_length):
     return (dx, dy)
 
 
-def clock_to_display():
+def draw_clock(draw):
+    now = datetime.datetime.now()
+    today_date = now.strftime("%b/%d/%y")
+    today_time = now.strftime("%H:%M:%S")
+    margin = 4
+    cx = 30
+    cy = min(64, 64) / 2
+    left = cx - cy
+    right = cx + cy
+    hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
+    hrs = posn(hrs_angle, cy - margin - 7)
+    min_angle = 270 + (6 * now.minute)
+    mins = posn(min_angle, cy - margin - 2)
+    sec_angle = 270 + (6 * now.second)
+    secs = posn(sec_angle, cy - margin - 2)
+    draw.ellipse((left + margin, margin, right - margin,
+                 min(128, 64) - margin), outline="white")
+    draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
+    draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
+    draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
+    draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2),
+                 fill="white", outline="white")
+    draw.text((2 * (cx + margin), cy - 8),
+              today_date, fill="yellow")
+    draw.text((2 * (cx + margin), cy), today_time, fill="yellow")
+
+
+def update_display():
     today_last_time = "Unknown"
     while True:
-        display_lock.acquire()
-        now = datetime.datetime.now()
-        today_date = now.strftime("%d %b %y")
-        today_time = now.strftime("%H:%M:%S")
-        if today_time != today_last_time:
-            today_last_time = today_time
-            with canvas(bz.display) as draw:
-                now = datetime.datetime.now()
-                today_date = now.strftime("%b/%d/%y")
-                margin = 4
-                cx = 30
-                cy = min(64, 64) / 2
-                left = cx - cy
-                right = cx + cy
-                hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
-                hrs = posn(hrs_angle, cy - margin - 7)
-                min_angle = 270 + (6 * now.minute)
-                mins = posn(min_angle, cy - margin - 2)
-                sec_angle = 270 + (6 * now.second)
-                secs = posn(sec_angle, cy - margin - 2)
-                draw.ellipse((left + margin, margin, right - margin,
-                             min(128, 64) - margin), outline="white")
-                draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
-                draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
-                draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
-                draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2),
-                             fill="white", outline="white")
-                draw.text((2 * (cx + margin), cy - 8),
-                          today_date, fill="yellow")
-                draw.text((2 * (cx + margin), cy), today_time, fill="yellow")
-        display_lock.release()
-        time.sleep(0.1)
+        if sleeping:
+            now = datetime.datetime.now()
+            today_time = now.strftime("%H:%M:%S")
+            if today_time != today_last_time:
+                today_last_time = today_time
+                display_lock.acquire()
+                with canvas(bz.display) as draw:
+                    draw_clock(draw)
+                display_lock.release()
+            time.sleep(0.1)
 
 
-clock_to_display_thread = Thread(target=clock_to_display)
-
+clock_to_display_thread = Thread(target=update_display)
 clock_to_display_thread.start()
