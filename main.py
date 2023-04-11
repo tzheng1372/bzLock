cb57c8b6b3dcf7615cf38c30f9471857deb121ef
Jason Jiannuo Pei <peijiannuo@gmail.com>
2023-04-10 23:48:47 -0400

    Add main.py with clock

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import math
-import time
-import datetime
+from threading import Lock, Thread
 from luma.core.render import canvas
+from bz import bzLock
 
-import bz
+import datetime
+import time
+import math
 
-bz = bz.bzLock()
+display_lock = Lock()
+
+bz = bzLock()
+
 bz.setup_display()
-device = bz.display
 
 
 def posn(angle, arm_length):
@@ -16,37 +19,31 @@ def posn(angle, arm_length):
     return (dx, dy)
 
 
-def main():
+def clock_to_display():
     today_last_time = "Unknown"
     while True:
+        display_lock.acquire()
         now = datetime.datetime.now()
         today_date = now.strftime("%d %b %y")
         today_time = now.strftime("%H:%M:%S")
         if today_time != today_last_time:
             today_last_time = today_time
-            with canvas(device) as draw:
+            with canvas(bz.display) as draw:
                 now = datetime.datetime.now()
                 today_date = now.strftime("%d %b %y")
-
                 margin = 4
-
                 cx = 30
-                cy = min(64, 64) / 2  # device.height
-
+                cy = min(64, 64) / 2
                 left = cx - cy
                 right = cx + cy
-
                 hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
                 hrs = posn(hrs_angle, cy - margin - 7)
-
                 min_angle = 270 + (6 * now.minute)
                 mins = posn(min_angle, cy - margin - 2)
-
                 sec_angle = 270 + (6 * now.second)
                 secs = posn(sec_angle, cy - margin - 2)
-
                 draw.ellipse((left + margin, margin, right - margin,
-                             min(128, 64) - margin), outline="white")  # device.height
+                             min(128, 64) - margin), outline="white")
                 draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
                 draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
                 draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
@@ -55,12 +52,10 @@ def main():
                 draw.text((2 * (cx + margin), cy - 8),
                           today_date, fill="yellow")
                 draw.text((2 * (cx + margin), cy), today_time, fill="yellow")
-
+        display_lock.release()
         time.sleep(0.1)
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
+clock_to_display_thread = Thread(target=clock_to_display)
+
+clock_to_display_thread.start()
